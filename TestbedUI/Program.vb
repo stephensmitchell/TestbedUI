bfb33877f5a2a5f6af90ce02d2472f89b5ac7c7b
Stephen S. Mitchell <stephen@stephensmitchell.com>
2024-05-17 17:50:12 -0500

    Refactored main method and UI window setup, updated help message
    Refactored the `Main` method to accept command line arguments and process them. Introduced a new method `ProcessCommand` to handle command processing. Refactored the code for starting a UI window into two new methods `StartUIWindow`. Updated the help message in the `ShowHelp` method to include the new command and adjusted the formatting.

--- TestbedUI/Program.vb
+++ TestbedUI/Program.vb
@@ -7,165 +7,104 @@ Module Program
     Dim grid As System.Windows.Controls.Grid
     Dim window As Window
     <STAThread>
-    Public Sub Main()
-        'TODO: Add mini logger
-        Dim keepRunning As Boolean = True
-        Console.WriteLine("Enter command ('/h' for list of commands):")
-        While keepRunning
-            Dim command As String = Console.ReadLine().ToLower()
-            Select Case command
-                Case "u1"
-                    Dim WindowTitleDesignName As String = "TestbedUI Design 01 - DEV MODE"
-                    Console.WriteLine(WindowTitleDesignName & " is ready...")
-                    Console.WriteLine(WindowTitleDesignName & " WindowState = WindowState.Minimized")
-                    Dim thread = New Thread(Sub()
-                                                SfSkinManager.ApplyStylesOnApplication = True
-                                                app = New Application()
-                                                window = New Window With {
-                                                    .Title = WindowTitleDesignName,
-                                                    .Width = 1920,
-                                                    .Height = 1080,
-                                                    .WindowStartupLocation = WindowStartupLocation.CenterScreen,
-                                                    .WindowState = WindowState.Minimized,
-                                                    .WindowStyle = WindowStyle.SingleBorderWindow,
-                                                    .Background = Brushes.Black
-                                                }
-                                                AddHandler window.Closed, AddressOf Window_Closed
-                                                grid = New System.Windows.Controls.Grid()
-                                                WindowLayout1.BuildGrid2(grid)
-                                                window.Content = grid
-                                                app.Run(window)
-                                            End Sub)
-                    thread.SetApartmentState(ApartmentState.STA)
-                    thread.Start()
-                Case "u2"
-                    Dim WindowTitleDesignName As String = "TestbedUI Design 02 - DEV MODE"
-                    Console.WriteLine(WindowTitleDesignName & " is ready...")
-                    Console.WriteLine(WindowTitleDesignName & " WindowState = WindowState.Minimized")
-                    Dim thread = New Thread(Sub()
-                                                SfSkinManager.ApplyStylesOnApplication = True
-                                                app = New Application()
-                                                window = New Window With {
-                                                    .Title = WindowTitleDesignName,
-                                                    .Width = 1920,
-                                                    .Height = 1080,
-                                                    .WindowStartupLocation = WindowStartupLocation.CenterScreen,
-                                                    .WindowState = WindowState.Minimized,
-                                                    .WindowStyle = WindowStyle.SingleBorderWindow,
-                                                    .Background = Brushes.Black
-                                                }
-                                                AddHandler window.Closed, AddressOf Window_Closed
-                                                grid = New System.Windows.Controls.Grid()
-                                                Dim newWebView As New Layout1()
-                                                ' grid.Children.Add(newWebView)
-                                                window.Content = newWebView
-                                                app.Run(window)
-                                            End Sub)
-                    thread.SetApartmentState(ApartmentState.STA)
-                    thread.Start()
-                Case "u3"
-                    Dim WindowTitleDesignName As String = "TestbedUI Design 03 - DEV MODE"
-                    Console.WriteLine(WindowTitleDesignName & " is ready...")
-                    Console.WriteLine(WindowTitleDesignName & " WindowState = WindowState.Minimized")
-                    Dim thread = New Thread(Sub()
-                                                SfSkinManager.ApplyStylesOnApplication = True
-                                                app = New Application()
-                                                window = New Window With {
-                                                    .Title = WindowTitleDesignName,
-                                                    .Width = 1920,
-                                                    .Height = 1080,
-                                                    .WindowStartupLocation = WindowStartupLocation.CenterScreen,
-                                                    .WindowState = WindowState.Minimized,
-                                                    .WindowStyle = WindowStyle.SingleBorderWindow,
-                                                    .Background = Brushes.Black
-                                                }
-                                                AddHandler window.Closed, AddressOf Window_Closed
-                                                grid = New System.Windows.Controls.Grid()
-                                                Dim newWebView As New Layout2()
-                                                ' grid.Children.Add(newWebView)
-                                                window.Content = newWebView
-                                                app.Run(window)
-                                            End Sub)
-                    thread.SetApartmentState(ApartmentState.STA)
-                    thread.Start()
-                Case "u4"
-                    Dim WindowTitleDesignName As String = "TestbedUI Design 04 - DEV MODE"
-                    Console.WriteLine(WindowTitleDesignName & " is ready...")
-                    Console.WriteLine(WindowTitleDesignName & " WindowState = WindowState.Minimized")
-                    Dim thread = New Thread(Sub()
-                                                SfSkinManager.ApplyStylesOnApplication = True
-                                                app = New Application()
-                                                window = New Window With {
-                                                    .Title = WindowTitleDesignName,
-                                                    .Width = 1920,
-                                                    .Height = 1080,
-                                                    .WindowStartupLocation = WindowStartupLocation.CenterScreen,
-                                                    .WindowState = WindowState.Minimized,
-                                                    .WindowStyle = WindowStyle.SingleBorderWindow,
-                                                    .Background = Brushes.Black
-                                                }
-                                                AddHandler window.Closed, AddressOf Window_Closed
-                                                grid = New System.Windows.Controls.Grid()
-                                                Dim newWebView As New Layout3()
-                                                ' grid.Children.Add(newWebView)
-                                                window.Content = newWebView
-                                                app.Run(window)
-                                            End Sub)
-                    thread.SetApartmentState(ApartmentState.STA)
-                    thread.Start()
-                Case "u5"
-                    Dim WindowTitleDesignName As String = "TestbedUI Design 05 - DEV MODE"
-                    Console.WriteLine(WindowTitleDesignName & " is ready...")
-                    Console.WriteLine(WindowTitleDesignName & " WindowState = WindowState.Minimized")
-                    Dim thread = New Thread(Sub()
-                                                SfSkinManager.ApplyStylesOnApplication = True
-                                                app = New Application()
-                                                window = New Window With {
-                                                    .Title = WindowTitleDesignName,
-                                                    .Width = 1920,
-                                                    .Height = 1080,
-                                                    .WindowStartupLocation = WindowStartupLocation.CenterScreen,
-                                                    .WindowState = WindowState.Minimized,
-                                                    .WindowStyle = WindowStyle.SingleBorderWindow,
-                                                    .Background = Brushes.Black
-                                                }
-                                                AddHandler window.Closed, AddressOf Window_Closed
-                                                grid = New System.Windows.Controls.Grid()
-                                                WindowLayout.BuildGrid(grid)
-                                                window.Content = grid
-                                                app.Run(window)
-                                            End Sub)
-                    thread.SetApartmentState(ApartmentState.STA)
-                    thread.Start()
-                Case "/r" 'TODO: Add restart functionality
-                    Console.WriteLine("Restarting the application...")
-                Case "/c"
-                    LoadCalca()
-                    RunNotepadAndWaitAsync()
-                    RunNotepadAndSendTextAsync()
-                Case "/h"
-                    ShowHelp()
-                Case "/e"
-                    Environment.Exit(0)
-                Case Else
-                    Console.WriteLine("Invalid command. Type 'help' to see the list of valid commands.")
-            End Select
-        End While
+    Public Sub Main(args As String())
+        If args.Length > 0 Then
+            ProcessCommand(args(0).ToLower())
+        Else
+            Console.WriteLine("Enter command ('/h' for list of commands):")
+            Dim keepRunning As Boolean = True
+            While keepRunning
+                Dim command As String = Console.ReadLine().ToLower()
+                ProcessCommand(command)
+            End While
+        End If
+    End Sub
+    Private Sub ProcessCommand(command As String)
+        Select Case command
+            Case "u1"
+                StartUIWindow("TestbedUI Design 01 - DEV MODE", AddressOf WindowLayout1.BuildGrid2)
+            Case "u2"
+                StartUIWindow("TestbedUI Design 02 - DEV MODE", Function() New Layout1())
+            Case "u3"
+                StartUIWindow("TestbedUI Design 03 - DEV MODE", Function() New Layout2())
+            Case "u4"
+                StartUIWindow("TestbedUI Design 04 - DEV MODE", Function() New Layout3())
+            Case "u5"
+                StartUIWindow("TestbedUI Design 05 - DEV MODE", AddressOf WindowLayout.BuildGrid)
+            Case "/r" 'TODO: Add restart functionality
+                Console.WriteLine("Restarting the application...")
+            Case "/c"
+                LoadCalca()
+                RunNotepadAndWaitAsync()
+                RunNotepadAndSendTextAsync()
+            Case "/h"
+                ShowHelp()
+            Case "/e"
+                Environment.Exit(0)
+            Case Else
+                Console.WriteLine("Invalid command. Type '/h' to see the list of valid commands.")
+        End Select
+    End Sub
+    Private Sub StartUIWindow(WindowTitleDesignName As String, layoutAction As Action(Of System.Windows.Controls.Grid))
+        Console.WriteLine($"{WindowTitleDesignName} is ready...")
+        Console.WriteLine($"{WindowTitleDesignName} WindowState = WindowState.Minimized")
+        Dim thread = New Thread(Sub()
+                                    SfSkinManager.ApplyStylesOnApplication = True
+                                    app = New Application()
+                                    window = New Window With {
+                                        .Title = WindowTitleDesignName,
+                                        .Width = 1920,
+                                        .Height = 1080,
+                                        .WindowStartupLocation = WindowStartupLocation.CenterScreen,
+                                        .WindowState = WindowState.Minimized,
+                                        .WindowStyle = WindowStyle.SingleBorderWindow,
+                                        .Background = Brushes.Black
+                                    }
+                                    AddHandler window.Closed, AddressOf Window_Closed
+                                    grid = New System.Windows.Controls.Grid()
+                                    layoutAction(grid)
+                                    window.Content = grid
+                                    app.Run(window)
+                                End Sub)
+        thread.SetApartmentState(ApartmentState.STA)
+        thread.Start()
+    End Sub
+    Private Sub StartUIWindow(WindowTitleDesignName As String, layoutControl As Func(Of System.Windows.Controls.UserControl))
+        Console.WriteLine($"{WindowTitleDesignName} is ready...")
+        Console.WriteLine($"{WindowTitleDesignName} WindowState = WindowState.Minimized")
+        Dim thread = New Thread(Sub()
+                                    SfSkinManager.ApplyStylesOnApplication = True
+                                    app = New Application()
+                                    window = New Window With {
+                                        .Title = WindowTitleDesignName,
+                                        .Width = 1920,
+                                        .Height = 1080,
+                                        .WindowStartupLocation = WindowStartupLocation.CenterScreen,
+                                        .WindowState = WindowState.Minimized,
+                                        .WindowStyle = WindowStyle.SingleBorderWindow,
+                                        .Background = Brushes.Black
+                                    }
+                                    AddHandler window.Closed, AddressOf Window_Closed
+                                    Dim newWebView = layoutControl()
+                                    window.Content = newWebView
+                                    app.Run(window)
+                                End Sub)
+        thread.SetApartmentState(ApartmentState.STA)
+        thread.Start()
     End Sub
     Private Sub ShowHelp()
-        ' This function shows the help instructions.
         Console.WriteLine("Available commands:")
         Console.WriteLine("  u1 - UI/Window Layout")
         Console.WriteLine("  u2 - UI/Window Layout")
         Console.WriteLine("  u3 - UI/Window Layout")
         Console.WriteLine("  u4 - UI/Window Layout")
+        Console.WriteLine("  u5 - UI/Window Layout")
         Console.WriteLine("  /r - Restarts the application (not implemented)")
         Console.WriteLine("  /c - Macros")
-        Console.WriteLine("  /h    - Shows this help message")
-        Console.WriteLine("  /e    - Kills the application")
+        Console.WriteLine("  /h - Shows this help message")
+        Console.WriteLine("  /e - Kills the application")
     End Sub
     Private Sub Window_Closed(sender As Object, e As EventArgs)
-        ' Check if there are no more windows open, then close the app
         If Application.Current.Windows.Count = 0 Then
             Application.Current.Shutdown()
             Environment.Exit(0)
